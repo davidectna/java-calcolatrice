d33474cca83b5edcdcfc9e79f75e13f5ebfe6f67
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-06-23 16:38:24 +0200

    Perfezionamento consegna base + bonus 1

--- src/calcolatrice/java/CalcoliHelper.java
+++ src/calcolatrice/java/CalcoliHelper.java
@@ -1,7 +1,14 @@
 package calcolatrice.java;
 
+/*BONUS 1: per ogni metodo scrivetene una versione che esegue le stesse operazioni ma su parametri double.
+Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi 
+di programmazione ad oggetti…Quale? Scrivetelo in un commento all’interno del vostro codice! :faccia_nerd:
+
+BONUS2: aggiungete il metodo per l’elevamento a potenza che prende come parametri due numeri interi (base ed esponente) 
+e ritorna la base elevata all’esponente. Occhio che sia la base che l’esponente potrebbero essere uguali a zero oppure numeri negativi!
+(caso esponente = 0 e base = 0 anche se in matematica sarebbe un numero indefinito usiamo l’approccio informatico che lo calcola come 1)*/
+
 public class CalcoliHelper {
-	
 	
 	private CalcoliHelper() {
 	//private constructor 
@@ -64,5 +71,66 @@ public class CalcoliHelper {
 		return minimumValue;
 	}
 	
+	//BONUS 1
+	// OVERLOAD 
+	
+		// sum
+		public static double numbersSum (double firstAddend,double secondAddend) {
+			return firstAddend + secondAddend;
+		}
+		//difference
+		public static double numbersDifference (double minuend, double subtrahend) {
+			return minuend - subtrahend;
+		}
+		//difference n.2
+		public static double numbersDifference2 (double minuend2, double subtrahend2) {
+			if (minuend2 < subtrahend2) {
+				return subtrahend2 - minuend2;
+				}
+			else {
+				return minuend2 - subtrahend2;
+			}
+		}
+		//multiplication
+		public static double numbersMultiplication (double firstFactor, double secondFactor) {
+			return firstFactor * secondFactor;
+		}
+		//absolute value
+		public static double absoluteValue (double firstNumber) {
+			
+			if (firstNumber < 0) {
+				firstNumber = -firstNumber;
+			}
+				return firstNumber;
+			}
+
+		
+		public static double minNumber (double secondNumber , double thirdNumber) {
+			
+			double minimumValue;
+			
+			if (secondNumber < thirdNumber) {
+				minimumValue = secondNumber;
+			}
+			else {
+				minimumValue = thirdNumber;
+			}
+			return minimumValue;
+		}
+		
+		
+		public static double maxNumber (double secondNumber , double thirdNumber) {
+			
+			double minimumValue;
+			
+			if (secondNumber > thirdNumber) {
+				minimumValue = secondNumber;
+			}
+			else {
+				minimumValue = thirdNumber;
+			}
+			return minimumValue;
+		}
+		
 	
 }
--- src/calcolatrice/java/CalcoliTest.java
+++ src/calcolatrice/java/CalcoliTest.java
@@ -2,14 +2,22 @@ package calcolatrice.java;
 
 import java.util.Random;
 
+/*BONUS 1: per ogni metodo scrivetene una versione che esegue le stesse operazioni ma su parametri double.
+Il fatto di dover scrivere lo stesso metodo per tipi di parametro diversi applica nella pratica uno dei principi 
+di programmazione ad oggetti…Quale? Scrivetelo in un commento all’interno del vostro codice! :faccia_nerd:
+
+BONUS2: aggiungete il metodo per l’elevamento a potenza che prende come parametri due numeri interi (base ed esponente) 
+e ritorna la base elevata all’esponente. Occhio che sia la base che l’esponente potrebbero essere uguali a zero oppure numeri negativi!
+(caso esponente = 0 e base = 0 anche se in matematica sarebbe un numero indefinito usiamo l’approccio informatico che lo calcola come 1)*/
+
 public class CalcoliTest {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Somma tra numeri :");
+		System.out.println("Differenza tra numeri con risultato negativo :");
 		System.out.println(CalcoliHelper.numbersDifference(5, 90));
 		
-		System.out.println("Differenza tra numeri :");
+		System.out.println("Differenza tra numeri con risultato obbligatoriamente positivo:");
 		System.out.println(CalcoliHelper.numbersDifference2(5, 94));
 		
 		System.out.println("Moltiplicazione tra numeri :");
