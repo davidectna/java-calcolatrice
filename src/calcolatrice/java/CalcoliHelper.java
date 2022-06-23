86a19ccc112a18ff5a57c6f043da3487fd13e4e5
davidectna@gmail.com <INDIRIZZO_E-MAIL>
2022-06-23 16:16:54 +0200

    Perfezionamento consegna

--- src/calcolatrice/java/CalcoliHelper.java
+++ src/calcolatrice/java/CalcoliHelper.java
@@ -1,14 +1,5 @@
 package calcolatrice.java;
 
-
-/*Creare una classe di helper CalcoliHelper. Come visto a lezione, strutturare la classe in modo 
- * che non possa essere istanziata (cioè non posso creare oggetti di tipo CalcoliHelper con la parola chiave new)
- *  e che presenti i seguenti metodi static:
-- Minimo tra due numeri interi
-- Massimo tra due numeri interi
-Create anche una classe CalcoliTest con il metodo main in cui testate tutti i metodi della vostra classe di helper 
-(con un esempio per ogni casistica)*/
-
 public class CalcoliHelper {
 	
 	
--- src/calcolatrice/java/CalcoliTest.java
+++ src/calcolatrice/java/CalcoliTest.java
@@ -6,18 +6,20 @@ public class CalcoliTest {
 
 	public static void main(String[] args) {
 		
-		
+		System.out.println("Somma tra numeri :");
 		System.out.println(CalcoliHelper.numbersDifference(5, 90));
 		
+		System.out.println("Differenza tra numeri :");
 		System.out.println(CalcoliHelper.numbersDifference2(5, 94));
 		
+		System.out.println("Moltiplicazione tra numeri :");
 		System.out.println(CalcoliHelper.absoluteValue(-12));
 		
 		Random random = new Random();
 		int randomNumber1 = random.nextInt((1000)+1);
 		int randomNumber2 = random.nextInt((1000)+1);
 		
-		System.out.println("I due numeri sono: "+ randomNumber1+ " e " +randomNumber2);
+		System.out.println("\n" + "I due numeri sono: "+ randomNumber1+ " e " +randomNumber2);
 		
 		System.out.println("Il numero minore è: " + CalcoliHelper.minNumber(randomNumber1, randomNumber2));
 		System.out.println("Il numero maggiore è: " + CalcoliHelper.maxNumber(randomNumber1, randomNumber2));
